541163fa511bcb9dc818fdd1841b8c351a287ee7
mensahchristabel <mensahchristabel623@gmail.com>
2026-08-02 22:45:18 +0000

    Update assignment_06_multiplication_table.cpp

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -57,3 +57,22 @@
 #include <iostream>
 using namespace std;
 
+
+// ===== PART A: Single Multiplication Table =====
+void printTable(int num) {
+    cout << "\nMultiplication Table for " << num << ":\n";
+    for (int i = 1; i <= 12; i++) {
+        cout << num << " x " << i << " = " << (num * i) << endl;
+    }
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    printTable(number); // Call the function
+    
+    return 0;
+}
+
